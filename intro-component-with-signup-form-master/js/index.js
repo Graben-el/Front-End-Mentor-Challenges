e481e92255fb9d3ad7f4b140a289122e818abd5b
Gabriel Rodrigues <gabriel.int96@gmail.com>
2022-03-15 21:02:01 -0300

    js refactor

--- intro-component-with-signup-form-master/js/index.js
+++ intro-component-with-signup-form-master/js/index.js
@@ -5,31 +5,10 @@ const password = document.querySelector('.password');
 const errorPacks = document.querySelectorAll('.hidden');
 const form = document.querySelector('.form');
 
-nome.addEventListener('focus', () => {
-    hideError(0)
-    nome.setAttribute('placeholder', 'First Name')
-    nome.value=''
-})
-
-sobreNome.addEventListener('focus', () => {
-    hideError(1)
-    sobreNome.setAttribute('placeholder', 'Last Name')
-    sobreNome.value=''
-})
-
-email.addEventListener('focus', () => {
-    hideError(2)
-    email.setAttribute('placeholder', 'E-mail')
-    email.value=''
-    
-})
-
-password.addEventListener('focus', () => {
-    hideError(3)
-    password.setAttribute('placeholder', 'Password')
-    password.value=''
-})
-
+addFocus(nome, 0, 'First Name')
+addFocus(sobreNome, 1, 'Last Name')
+addFocus(email, 2, 'E-mail')
+addFocus(password, 3, 'Password')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -37,44 +16,52 @@ form.addEventListener('submit', (e) => {
     if (nome.value.length === 0) {
         showError(0, nome)
     } else {
-        hideError(0)
-        cleanForm(nome)
+        hideNClean(0, nome)
     }
 
     if (sobreNome.value.length === 0) {
         showError(1, sobreNome)
     } else {
-        hideError(1)
-        cleanForm(sobreNome)
+        hideNClean(1, sobreNome)
     }
 
     if (email.value.length === 0 || email.value.indexOf('@') === -1 || email.value.indexOf('.') === -1) {
         showError(2, email) 
     } else {
-        hideError(2)
-        cleanForm(email)
+        hideNClean(2, email)
     }
 
     if (password.value.length === 0) {
         showError(3, password)
     } else {
-        hideError(3)
-        cleanForm(password)
-
+        hideNClean(3, password)
     }
-    
 })
 
+
+
+function addFocus(input, errorIndex, placeholder){
+    input.addEventListener('focus', () => {
+        hideError(errorIndex)
+        input.setAttribute('placeholder', `${placeholder}`)
+        input.value=''
+    })
+}
 
 function hideError(index) {
     return errorPacks[index].style.display = 'none'
 }
 
 function showError(index, input) {
-    errorPacks[index].style.display = 'flex';
+    errorPacks[index].style.display = 'flex'
     input.value = ''
     input.setAttribute('placeholder', '')
-    form.style.height = '118%'; 
+    form.style.height = '118%'
+}
+
+function hideNClean(index, input) {
+    hideError(index)
+    cleanForm(input)
 }
 
 function cleanForm(input) {
